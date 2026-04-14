794e59c7e47a3f2371878a5c2cf748edb4ac10e2
DaniloGesso <danilogessocomqualidade@gmail.com>
2026-04-14 20:51:48 -0300

    Refactor WhatsApp functions and add gallery feature

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
-function enviarWhats(servico) {
-    let numero = "62991889486"; // SEU NÚMERO
-
-    let mensagem = `Olá, gostaria de um orçamento para ${servico}`;
-    let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
-
-    window.open(url, "_blank");
+function irServicos() {
+    window.location.href = "servicos.html";
 }
 
 function abrirOpcoes() {
-    let servico = prompt(
-        "Digite o serviço:\n\nRebaixamento\nForro\nGesso\nMolduras\nCortineiras\nSancas\nDivisórias\nReparos"
-    );
+    document.getElementById("modal").style.display = "flex";
+}
 
-    if (servico) {
-        enviarWhats(servico);
+window.onclick = function(e) {
+    let modal = document.getElementById("modal");
+    if (e.target === modal) {
+        modal.style.display = "none";
     }
 }
 
-function abrirImagem(img) {
-    document.getElementById("modal").style.display = "flex";
-    document.getElementById("imgGrande").src = img.src;
+function enviarWhats() {
+    let servico = document.getElementById("servico").value;
+    let msg = `Olá, quero orçamento para ${servico}`;
+    window.open(`https://wa.me/62991889486?text=${encodeURIComponent(msg)}`);
 }
 
-function fecharImagem() {
-    document.getElementById("modal").style.display = "none";
+function abrirGaleria(servico) {
+
+    document.getElementById("galeria").style.display = "block";
+
+    let imagens = document.getElementById("imagens");
+    imagens.innerHTML = "";
+
+    // QUANTIDADE DE FOTOS (VOCÊ CONTROLA AQUI)
+    let total = 11; // pode mudar pra 10, 20, etc
+
+    for (let i = 1; i <= total; i++) {
+        let img = document.createElement("img");
+        img.src = `img/${servico}${i}.jpg`;
+        imagens.appendChild(img);
+    }
+
+    let msg = `Olá, quero orçamento de ${servico}`;
+    document.getElementById("whats").href =
+        `https://wa.me/62991889486?text=${encodeURIComponent(msg)}`;
+}
+function fecharGaleria() {
+    document.getElementById("galeria").style.display = "none";
 }
 
 function enviarAvaliacao() {
-    alert("Obrigado pela sua avaliação!");
+    let texto = document.getElementById("avaliacaoTexto").value;
+    if (texto === "") return;
+
+    let p = document.createElement("p");
+    p.innerText = "⭐ ⭐ ⭐ ⭐ ⭐ - " + texto;
+
+    document.getElementById("listaAvaliacoes").appendChild(p);
+    document.getElementById("avaliacaoTexto").value = "";
 }
